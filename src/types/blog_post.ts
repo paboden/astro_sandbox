3e22361b520fd09a861e8dd4f7487c0f59be0549
Philip Boden <philip.boden@gmail.com>
2025-10-14 20:28:25 -0700

    working trying to get content collection reference working

--- src/types/blog_post.ts
+++ src/types/blog_post.ts
@@ -1,4 +1,4 @@
-import { z } from 'astro:content';
+import { z, type CollectionEntry, reference } from 'astro:content';
 
 export const BlogPostSchema = z.object({
   id: z.number(),
@@ -8,7 +8,7 @@ export const BlogPostSchema = z.object({
   tags: z.array(z.string()).nullish(),
   summary: z.string().nullish(),
   image: z.string().nullish(),
-  author: z.string().nullish(),
+  authors: z.array(z.string()).nullish(),
 })
 
 export type BlogPostSchema = z.infer<typeof BlogPostSchema>
