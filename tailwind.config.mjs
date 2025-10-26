f263de09bddcca6f2d6d4b6ac8108ffe68a401c1
Philip Boden <philip.boden@gmail.com>
2025-10-25 22:24:42 -0700

    Image work and playing with a grid setup for park page header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,14 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'parks-header-main': '[full-start] minmax(1em, 1fr) [main-start] minmax(0, 40rem) [main-end] minmax(0, 1rem) [side-start] minmax(0, 20rem) [side-end] minmax(1em, 1fr) [full-end]',
+      },
+      gridTemplateAreas: {
+        'parks-header-main': '[full-start] minmax(1em, 1fr) [main-start] minmax(0, 40rem) [main-end] minmax(0, 1rem) [side-start] minmax(0, 20rem) [side-end] minmax(1em, 1fr) [full-end]',
+      },
+    },
   },
   plugins: [],
 };
